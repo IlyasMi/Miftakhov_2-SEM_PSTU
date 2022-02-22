b0e6734c440d85ecd98a5e42edb40c0550aab11d
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2022-02-22 23:49:13 +0500

    Add files via upload

---
+++ Ханой/Hanoi.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+using namespace std;
+int hanoi(int n, int i, int k)
+{
+	if (n == 1)
+	{
+		cout << n << "," << i << "," << k << endl;
+	}
+	else
+	{
+		int tmp = 6 - i - k;
+		hanoi(n - 1, i, tmp);
+		cout << n << "," << i << "," << k << endl;
+		hanoi(n - 1, tmp, k);
+	}
+	return 2;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	hanoi(3, 1, 2);
+
+
+	return 0;
+}
