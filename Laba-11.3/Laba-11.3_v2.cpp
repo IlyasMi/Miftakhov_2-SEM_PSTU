89368cd1a4093e650566d7034e108faea3dad6a5
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2022-03-10 18:36:59 +0500

    Add files via upload

---
+++ Laba-11.3/Laba-11.3_v2.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<queue>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	priority_queue<char> q;
+	cout << "Введите длину очереди: ";
+	int n;
+	cin >> n;
+	cout << endl;
+	for (int i = 1; i <= n; i++) 
+	{
+		cout << "Введите элемент очереди: ";
+		char a;
+		cin >> a;
+		q.push(a);  // добавляем в очередь элементы
+		cout << endl;
+	}
+	cout << endl;
+	if (!q.empty()) cout << "Очередь не пуста!";  // проверяем пуста ли очередь (нет)
+	cout << endl;
+	for (int i = 0; i <= n/2; i++)
+	{
+		cout << "Введите элемент очереди: ";
+		char a;
+		cin >> a;
+		q.push(a);  // добавляем в очередь элементы
+		cout << endl;
+	}
+	while (!q.empty()) {
+		cout << q.top() << " ";
+		q.pop();
+	}
+	return 0;
+}
