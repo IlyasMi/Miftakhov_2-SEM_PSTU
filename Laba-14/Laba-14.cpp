756f408bcb8eb759b8feb5b1a193972db2efaa1c
Ilyas_Miftakhov <91590896+IlyasMi@users.noreply.github.com>
2022-03-13 21:37:17 +0500

    Add files via upload

---
+++ Laba-14/Laba-14.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	int tmp = 0;
+	int arr[5] = { 34, 213, 99, 0, 515 };
+	cout << "Введите номер элемента для замены от 0 до 4: ";
+	cin >> n;
+	cout << endl;
+	if (n != 4)
+		{
+			
+				tmp = arr[n];
+				arr[n] = arr[n + 1];
+				arr[n + 1] = tmp;
+			
+		}
+		else if (n == 4)
+		{
+			
+				tmp = arr[n];
+				arr[n] = arr[n - 1];
+				arr[n - 1] = tmp;
+			
+		}
+	for (int i = 0; i < 5; i++)
+	{
+		cout << arr[i] << " ";
+	}
+
+	return 0;
+}
